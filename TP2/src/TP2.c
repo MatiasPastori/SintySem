599fb91336237c128a423cadb930a6a65c051756
Matias Pastori <matiaspastori97@gmail.com>
2019-04-22 15:50:42 -0300

    Prueba

---
+++ TP2/src/TP2.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : TP2.c
+ Author      : SintySem
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("TP ANUAL ENTREGA 2 Sintaxis UTN FRBA 2019"); /* prints TP ANUAL ENTREGA 2 Sintaxis UTN FRBA 2019 */
+	return EXIT_SUCCESS;
+}
