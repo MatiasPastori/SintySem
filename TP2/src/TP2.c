21c84008752ccd1f3f0f7c58a6cc807d4701b655
matup <matup@DESKTOP-CQ2EHL2>
2019-05-06 21:23:19 -0300

    Test

--- TP2/src/TP2.c
+++ TP2/src/TP2.c
@@ -2,9 +2,9 @@
  ============================================================================
  Name        : TP2.c
  Author      : SintySem
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
+ Version     : 1.0
+ Copyright   : >:C
+ Description : TP ANUAL ENTREGA 2 Sintaxis UTN FRBA 2019"
  ============================================================================
  */
 
@@ -12,6 +12,6 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("TP ANUAL ENTREGA 2 Sintaxis UTN FRBA 2019"); /* prints TP ANUAL ENTREGA 2 Sintaxis UTN FRBA 2019 */
+
 	return EXIT_SUCCESS;
 }
